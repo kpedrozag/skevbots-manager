98d450d4404c6c8e00e028f15e9958cea6f3206e
Kevin Pedroza Goenaga <kevinpedroza26@gmail.com>
2024-12-08 17:58:25 -0500

    chore(db): Capitalize process names

--- db/image/mongo-init.js
+++ db/image/mongo-init.js
@@ -5,52 +5,52 @@ db.tasks.createIndex({ id: 1 }, { unique: true });
 db.tasks.insertMany([
   {
     "id": 10001,
-    "description": "process invoice",
+    "description": "Process invoice",
     "duration": 1500
   },
   {
     "id": 10002,
-    "description": "categorize transactions",
+    "description": "Categorize transactions",
     "duration": 2750
   },
   {
     "id": 10003,
-    "description": "extract statement data",
+    "description": "Extract statement data",
     "duration": 18000
   },
   {
     "id": 10004,
-    "description": "amortize loan payments",
+    "description": "Amortize loan payments",
     "duration": 2573
   },
   {
     "id": 10005,
-    "description": "depreciate fixed assets",
+    "description": "Depreciate fixed assets",
     "duration": 7454
   },
   {
     "id": 10006,
-    "description": "calculate payroll allocations",
+    "description": "Calculate payroll allocations",
     "duration": 37473
   },
   {
     "id": 10007,
-    "description": "generate month end report",
+    "description": "Generate month end report",
     "duration": 34332
   },
   {
     "id": 10008,
-    "description": "audit for discrepancies",
+    "description": "Audit for discrepancies",
     "duration": 6765
   },
   {
     "id": 10009,
-    "description": "close the books",
+    "description": "Close the books",
     "duration": 3643
   },
   {
     "id": 10010,
-    "description": "perform reconciliation",
+    "description": "Perform reconciliation",
     "duration": 4542
   }
 ]);
